a9e6771f7c5110bdacf71c8db28150068962ffe0
Daniel Branco <dbranco@gmail.com>
2021-09-09 23:42:20 +0100

    :white_check_mark: Add the code to make the java sample to work
    Added the needed code and documentation to make the Java sample to work in AWS Lambda
    
    Co-authored-by: Daniel Branco <dbranco@gmail.com>

--- java-aws-lambda/src/main/java/com/example/javaawslambda/JavaAwsLambdaApplication.java
+++ java-aws-lambda/src/main/java/com/example/javaawslambda/JavaAwsLambdaApplication.java
@@ -2,12 +2,20 @@ package com.example.javaawslambda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.function.Function;
 
 @SpringBootApplication
 public class JavaAwsLambdaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaAwsLambdaApplication.class, args);
+	}
+
+	@Bean
+	public Function<String, String> greet() {
+		return input -> "Hello from java " + input;
 	}
 
 }
